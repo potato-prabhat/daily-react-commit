e8a14657ada78d6b7895776e4a07180968b1f010
Prabhat <krsinghp8@gmail.com>
2024-04-26 03:09:15 -0400

    07

---
+++ my-app/src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    author: 'Emily Henry',
+    title: 'Funny Story',
+    img: 'https://m.media-amazon.com/images/I/91Yb6fD29xL._AC_UY218_.jpg',
+    id: 1,
+  },
+  {
+    author: 'Ann Napolitano',
+    title: 'Hello Beautiful',
+    img: 'https://m.media-amazon.com/images/I/91ejqv4ZJ6L._AC_UY218_.jpg',
+    id: 2,
+  },
+]
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,57 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-const books = [
-  {
-    author: 'Emily Henry',
-    title: 'Funny Story',
-    img: 'https://m.media-amazon.com/images/I/91Yb6fD29xL._AC_UY218_.jpg',
-    id: 1,
-  },
-  {
-    author: 'Ann Napolitano',
-    title: 'Hello Beautiful',
-    img: 'https://m.media-amazon.com/images/I/91ejqv4ZJ6L._AC_UY218_.jpg',
-    id: 2,
-  },
-]
+import { books } from './books'
+import Book from './Book'
 
 const BookList = () => {
   const someValue = 'shakeAndBake'
   const displayValue = () => {
     console.log(someValue)
   }
-  const getBook = (id) => {
-    const bookFound = books.find((book) => book.id === id)
-    console.log(bookFound)
-  }
 
   return (
     <section className="booklist">
       {books.map((book) => {
-        return (
-          <Book
-            {...book}
-            key={book.id}
-            displayValue={displayValue}
-            getBook={getBook}
-          />
-        )
+        return <Book {...book} key={book.id} displayValue={displayValue} />
       })}
     </section>
-  )
-}
-
-const Book = (props) => {
-  const { id, img, title, author, getBook } = props
-
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={() => getBook(id)}>click me</button>
-      <h4>{author.toUpperCase()}</h4>
-    </article>
   )
 }
 
